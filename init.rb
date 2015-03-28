3ce9f239bfa8574c9bd0a6f902627b77ca73deab
Eugene Jakovlev <eugene.jakovlev@gmail.com>
2015-03-28 14:49:39 +0300

    Init file our store

---
+++ init.rb
@@ -0,0 +1,15 @@
+require_relative "item"
+require_relative "real_item"
+require_relative "virtual_item"
+require_relative "cart"
+
+item1 = VirtualItem.new({:price => 10, :weight => 300, :name => "Car" })
+item2 = RealItem.new({:weight => 300, :name => "Car" })
+
+
+cart = Cart.new
+cart.add_item item1
+cart.add_item item2
+
+puts item1.price
+puts item1.real_price
