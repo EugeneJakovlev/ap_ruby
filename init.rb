415beb1646acea1271a2735126ff7a609f855db6
Eugene Jakovlev <eugene.jakovlev@gmail.com>
2015-03-28 16:49:16 +0300

    Test our changes (in order.rb init.rb item.rb

--- init.rb
+++ init.rb
@@ -1,15 +1,24 @@
+require_relative "item_container"
+require_relative "cart"
+require_relative "order"
 require_relative "item"
 require_relative "real_item"
 require_relative "virtual_item"
-require_relative "cart"
+
 
 item1 = VirtualItem.new({:price => 10, :weight => 300, :name => "Car" })
-item2 = RealItem.new({:weight => 300, :name => "Car" })
+item2 = RealItem.new({:price => 100, :weight => 300, :name => "Car" })
 
 
 cart = Cart.new
 cart.add_item item1
 cart.add_item item2
+puts cart.items.size
 
-puts item1.price
-puts item1.real_price
+
+order = Order.new
+order.add_item item1
+order.add_item item2
+puts order.items.size
+
+puts order.count_valid_items
