ea361e6d82f669dc02bf3ee0c88aaa885d5cda42
Eugene Jakovlev <eugene.jakovlev@gmail.com>
2015-03-29 00:57:54 +0300

    Edit VirtualItem >> RealItem

--- init.rb
+++ init.rb
@@ -1,3 +1,4 @@
+require_relative "string"
 require_relative "item_container"
 require_relative "cart"
 require_relative "order"
@@ -5,22 +6,7 @@ require_relative "item"
 require_relative "real_item"
 require_relative "virtual_item"
 
-
-item1 = VirtualItem.new({:price => 101, :weight => 300, :name => "Audi" })
-item2 =    RealItem.new({:price => 101, :weight => 300, :name => "BMW" })
-item3 = VirtualItem.new({:price => 11, :weight => 300, :name => "Opel" })
-
-
-
-cart = Cart.new
-cart.add_item item1
-cart.add_item item3
-cart.add_item item2
-puts "--------------------"
-puts cart.items.size
-puts "--------------------"
-print item1.name + " " + item1.price.to_s + " " + item1.real_price.to_s+"\n"
-puts "--------------------"
-print item2.name + " " + item2.price.to_s + " " + item2.real_price.to_s+"\n"
-puts "--------------------"
-print item3.name + " " + item3.price.to_s + " " + item3.real_price.to_s+"\n"
+@items = []
+@items << RealItem.new({:price => 101, :weight => 300, :name => "audi" })
+@items << RealItem.new({:price => 131, :weight => 300, :name => "opel" })
+@items << RealItem.new({:price => 101, :weight => 300, :name => "bmw" })
